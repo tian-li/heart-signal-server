5c3f8d4d663861bee56929ca7a135c01f49c8b62
Tian <litiansq89@gmail.com>
2020-11-18 19:19:36 -0600

    port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: PORT=443 node index.js
+web: node index.js
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {});
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
